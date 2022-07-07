ca2912f1b855c32e34536ece6083e11d303c2669
Sati <rock_sal@mail.ru>
2022-07-07 16:20:27 +0600

    Automated questions and also made checks for them

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* Задание на урок:
+/* Задание на урок 1:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
 'Сколько фильмов вы уже посмотрели?'
@@ -21,7 +21,23 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-"use strict";
+/* Задание на урок 2:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами*/
+
+// Код возьмите из предыдущего домашнего задания
+
+'use strict';
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -33,12 +49,64 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = +prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = +prompt('На сколько оцените его?', '');
+if (personalMovieDB.count <= 10) {
+    alert('Просмотрено довольно мало фильмов');
+}
+else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30 ) {
+    alert('Вы классический зритель');
+}
+else if (personalMovieDB.count > 30) {
+    alert('Вы киноман');
+}
+else {
+    alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// I
+// let autoQuestions = 0;
+
+// while (autoQuestions < 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = +prompt('На сколько оцените его?', '');
+
+//     autoQuestions++;
+
+//     if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         autoQuestions--;
+//     }
+// }
+
+// II
+// let autoQuestions = 0;
+
+// do {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = +prompt('На сколько оцените его?', '');
+
+//     autoQuestions++;
+
+//     if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//     } else {
+//         autoQuestions--;
+//     }
+// } while (autoQuestions < 2);
+
+
+
+// III
+
+for (let autoQuestions = 0; autoQuestions < 2; autoQuestions++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = +prompt('На сколько оцените его?', '');
+
+    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        autoQuestions--;
+    }
+}
 
 console.log(personalMovieDB);
